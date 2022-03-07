9f0af5051815508675fa1049bd92b2edab20c163
Gabriel Assana <gabrielhhz2@gmail.com>
2022-03-07 19:49:27 -0300

    finalizando estilização dos botões de tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { Container, TransactionTypeContainer } from './style';
+import { Container, RadioBox, TransactionTypeContainer } from './style';
 
 
 interface NewTransactionModalProps {
@@ -13,6 +14,9 @@ interface NewTransactionModalProps {
 
 
 export function NewTransactionModal({isOpen, onRequestClose }:NewTransactionModalProps) {
+
+    const [type, setType] = useState('deposit')
+
     return(
         <Modal 
           isOpen={isOpen}
@@ -43,19 +47,25 @@ export function NewTransactionModal({isOpen, onRequestClose }:NewTransactionModa
 
             <TransactionTypeContainer>
                 
-                <button
+                <RadioBox
                    type="button"
+                   onClick={() => { setType('deposit') }}
+                   isActive={type === 'deposit'}
+                   activeColor="green" 
                 >
                     <img src={incomeImg} alt="Entrada"/>
                     <span>Entrada</span>
-                </button>
+                </RadioBox>
 
-                <button
+                <RadioBox
                    type="button"
+                   onClick={() => { setType('withdraw') }}   
+                   isActive={type === 'withdraw'}    
+                   activeColor="red"            
                 >
                     <img src={outcomeImg} alt="Saída"/>
                     <span>Saída</span>
-                </button>
+                </RadioBox>
                 
             </TransactionTypeContainer>
 
